99f4ae79e377df2690f8b3e06efb5d87864a0e15
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-01-30 12:27:59 +0500

    refactor: типизировать Header

--- movies-scss-redux-ts/src/components/Header/Header.tsx
+++ movies-scss-redux-ts/src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navigation from '../Navigation/Navigation';
-import { RootState } from '../../store';
+import type { RootState } from '../../store';
+type TypeLocation = {
+  hash: string;
+  key: string;
+  pathname: string;
+  search: string;
+  state: string | null;
+}
 
 export default function Header() {  
-  const loggedIn = useSelector((state: RootState) => state.logged.loggedIn);
-  const { pathname } = useLocation();
-  const isMain = pathname === '/';
-  const isMovies = pathname === '/movies';
-  const isSavedMovies = pathname === '/saved-movies';
-  const isProfile = pathname === '/profile';
+  const loggedIn: boolean = useSelector((state: RootState) => state.logged.loggedIn);
+  const { pathname }: TypeLocation = useLocation();  
+  const isMain: boolean = pathname === '/';
+  const isMovies: boolean = pathname === '/movies';
+  const isSavedMovies: boolean = pathname === '/saved-movies';
+  const isProfile: boolean = pathname === '/profile';
 
   return (
     <header className={`header ${!isMain && "header_light"}`}>
