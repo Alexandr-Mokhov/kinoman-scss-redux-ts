2aee494c9ec0c8c486030bf14070608c5739be08
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-01-31 21:26:14 +0500

    refactor: типизировать слайс обработки ошибок и добавить стили для Sass

--- src/store/features/errorSlice.ts
+++ src/store/features/errorSlice.ts
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+type TypeInitialState = {
+  errorText: string
+}
+
+const initialState: TypeInitialState = {
   errorText: '', 
 }
 
