06c237a8ce2c88a859f03d2841340f161e14b11f
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-02-14 12:57:41 +0500

    refactor: типизировать компонент ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,8 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { RootState } from "../../../types";
 
-const ProtectedRouteElement = ({ element: Component, ...props }) => {
-  const loggedIn = useSelector(state => state.logged.loggedIn);
+const ProtectedRouteElement = ({ element: Component, ...props }: {element: React.FC}) => {
+  const loggedIn = useSelector((state: RootState) => state.logged.loggedIn);
 
   return (
     loggedIn ? <Component {...props} /> : <Navigate to="/" replace />
