5310191edda2af1ab97ccc9d1962189067756ee2
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-02-16 16:56:45 +0500

    refactor: удалить файлы старого проекта

--- movies-scss-redux/src/pages/movies/MoreMovies/MoreMovies.js
+++
@@ -1,9 +0,0 @@
-export default function MoreMovies({ handleClickMore }) {
-  return (
-    <section className="more-movies">
-      <div className="more-movies__container">
-        <button className="more-movies__button" type="button" onClick={handleClickMore}>Еще</button>
-      </div>
-    </section>
-  )
-}
